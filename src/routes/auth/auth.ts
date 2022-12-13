d85e2decf4e107664a136ac893a1c5546ff9ba25
Mario Josue Velasquez Banegas <MVelasquezB@ransa.net>
2022-12-13 16:58:54 -0600

    32vo commit

--- src/routes/auth/auth.ts
+++ src/routes/auth/auth.ts
@@ -10,10 +10,6 @@ import { dataApi, loginI } from '../../interfaces/dataApi';
 const app: Router = Router();
 const SECRET_KEY = 'wv+BWnX\qRap|S[%f/jd!?pq*O[Mg$Z+|/g/c;(]X(O%CB[Y[[&Gi)z##WEHP?u6<a0f_h3Q%V}_!>N<DElTSi>t=!R{jMtu}j/Eu5Xrb*x>)1$50G|,:xg+Y1OM}H183.b5MNH>5L-7u7tu<Dyc,JHDuqHZy68f71kFBjI4w|T\!{';
 
-
-
-
-
 app.get('/categorias', (req: Request, res:Response)=>{
 
 
